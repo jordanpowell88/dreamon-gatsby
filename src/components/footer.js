38f967cf4a7b0d55cc897038424c7330d0d73740
jordanpowell88 <jordanpowell@jordanpwellsmbp.lan>
2020-03-02 09:53:25 -0500

    feat: add hubspot integration

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Helmet from 'react-helmet';
 
 const Footer = () => {
     const date = new Date().getFullYear()
     return (
-    <footer className="footer-area parallax">
-        <div className="main-footer-area">
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12 text-center">
-                        <div className="copy-right">
-                            <p>Copyright &copy; {date} <Link to="/">| Dream On: Global |</Link> all rights reserved.</p>
+    <>
+        <footer className="footer-area parallax">
+            <div className="main-footer-area">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 text-center">
+                            <div className="copy-right">
+                                <p>Copyright &copy; {date} <Link to="/">| Dream On: Global |</Link> all rights reserved.</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+        <Helmet>
+            <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/7228931.js"></script>
+        </Helmet>
+    </>
     )
 }
 export default Footer;
